4d3f50fe4062b1e465c95491989340a43fc2b7dc
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-03-18 16:07:10 +0300

    Refactor app structure: replace App.js with App.jsx, add Header, Main, DateCounter, Error, Loader components, and update index.html title

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question?</p>
+      </Main>
+    </div>
+  );
+}
+
+export default App;
