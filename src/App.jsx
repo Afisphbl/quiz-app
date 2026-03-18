30e2868b95272ad1392a5c27caa9d2f140d1681d
Abduselam Seid <abuabdurehman0308@gmail.com>
2026-03-18 16:23:07 +0300

    Add questions data and implement data fetching in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      return state;
+  }
+};
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
